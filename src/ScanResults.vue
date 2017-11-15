8c619a8300883edd09ba604926037c02c5737109
FliiFe <theophile.cailliau@gmail.com>
2017-11-15 21:35:02 +0100

    Support for scanning pages

--- src/ScanResults.vue
+++ src/ScanResults.vue
@@ -5,50 +5,50 @@
                 <v-flex xs12>
                     <v-card if="upcard">
                         <v-card-text>
-                            <p>Les fichiers doivent être au format <code>.jpg</code> ou <code>.png</code></p>
+                            <p>Les fichiers doivent être des images (<code>.png</code>, <code>.jpg</code>, ...). Les feuilles peuvent être scannées à l'envers.</p>
                             <form ref="fiform">
-                                <input type="file" name="filelist" id="filelist" @change="filechange" ref="fileinput" />
+                                <input type="file" name="filelist" id="filelist" accept="image/*" @change="filechange" ref="fileinput" multiple/>
                             </form>
                         </v-card-text>
                     </v-card>
                 </v-flex>
-                <v-flex d-flex xs12 md6>
-                    <v-card>
+                <v-flex d-flex xs12 md4>
+                    <v-card height="fit-content">
                         <v-card-title class="title">File d'attente</v-card-title>
                         <v-card-text>
                             <v-data-table
                                     v-bind:headers="fileheaders"
-                                    :items="files"
+                                    no-data-text="Liste vide"
+                                    :items="files.filter(f => f.status !== 2)"
                                     hide-actions
-                                    class="elevation-1"
-                                >
+                                    class="elevation-0" >
                                 <template slot="items" scope="props">
                                     <td>{{ props.item.filename }}</td>
-                                    <td class="text-xs-right">{{ props.item.loading }}</td>
+                                    <td class="text-xs-right">
+                                        <v-progress-circular indeterminate color="primary" v-if="props.item.status"></v-progress-circular>
+                                        <v-icon v-else>schedule</v-icon>
+                                    </td>
                                 </template>
                             </v-data-table>
                         </v-card-text>
                     </v-card>
                 </v-flex>
-                <v-flex d-flex xs12 md6>
+                <v-flex d-flex xs12 md8>
                     <v-card>
                         <v-card-title class="title">Résultats</v-card-title>
                         <v-card-text>
                             <v-data-table
-                                    v-bind:headers="headers"
-                                    :items="items"
+                                    v-bind:headers="resultheaders"
+                                    no-data-text="Liste vide"
+                                    :items="results"
                                     hide-actions
-                                    class="elevation-1"
-                                >
+                                    class="elevation-0" >
                                 <template slot="items" scope="props">
-                                    <td>{{ props.item.name }}</td>
-                                    <td class="text-xs-right">{{ props.item.calories }}</td>
-                                    <td class="text-xs-right">{{ props.item.fat }}</td>
-                                    <td class="text-xs-right">{{ props.item.carbs }}</td>
-                                    <td class="text-xs-right">{{ props.item.protein }}</td>
-                                    <td class="text-xs-right">{{ props.item.sodium }}</td>
-                                    <td class="text-xs-right">{{ props.item.calcium }}</td>
-                                    <td class="text-xs-right">{{ props.item.iron }}</td>
+                                    <td>{{ props.item.category }}</td>
+                                    <td class="text-xs-right">{{ props.item.sex }}</td>
+                                    <td class="text-xs-right">{{ props.item.name }}</td>
+                                    <td class="text-xs-right">{{ props.item.voies }}</td>
+                                    <td class="text-xs-right">{{ props.item.blocs }}</td>
                                 </template>
                             </v-data-table>
                         </v-card-text>
@@ -60,19 +60,18 @@
 </template>
 <script>
 import toBase64 from 'arraybuffer-base64';
+import ImageCompressor from '@xkeshi/image-compressor';
 
 export default {
     name: 'scan-results',
     data () {
         return {
-            scans: [],
-            currentstep: 1,
-            loading: false,
             fileheaders: [
                 {
                     text: 'Nom du fichier',
                     value: 'filename',
-                    align: 'left'
+                    align: 'left',
+                    sortable: false
                 },
                 {
                     text: 'Status',
@@ -80,12 +79,31 @@ export default {
                     align: 'right'
                 }
             ],
-            files: [
+            resultheaders: [
                 {
-                    filename: 'lol.png',
-                    loading: 0
+                    text: 'Catégorie',
+                    value: 'category',
+                    align: 'left'
+                },{
+                    text: 'Sexe',
+                    value: 'sex',
+                    align: 'right'
+                },{
+                    text: 'Nom',
+                    value: 'name',
+                    align: 'right'
+                },{
+                    text: 'Voies',
+                    value: 'voies',
+                    align: 'right'
+                },{
+                    text: 'Blocs',
+                    value: 'blocs',
+                    align: 'right'
                 }
-            ]
+            ],
+            results: [],
+            files: []
         };
     },
     computed: {
@@ -101,32 +119,56 @@ export default {
             console.log('added file');
             const form = this.$refs.fiform;
             Array.prototype.forEach.call(input.files, file => {
-                if(file.name.endsWith('jpg') || file.name.endsWith('png') || file.name.endsWith('pdf')) {
-                    const reader = new FileReader();
-                    reader.onload = (r) => this.scans.push({
-                        filename: file.name,
-                        content: toBase64(r.target.result)
-                    });
-                    reader.readAsArrayBuffer(file);
-                } else {
-                    setTimeout(() => alert('Type de fichier incompatible'), 1000);
-                }
-                form.reset();
+                new ImageCompressor(file, {
+                    quality: .9,
+                    maxWidth: 620,
+                    success: (res) => {
+                        const reader = new FileReader();
+                        reader.onload = (r) => {
+                            this.files.push({
+                                filename: res.name,
+                                content: toBase64(r.target.result),
+                                status: 0
+                            });
+                            this.updateQueue();
+                        };
+                        reader.readAsArrayBuffer(res);
+                    }
+                });
             });
+            form.reset();
         },
-        submit() {
-            if(this.loading) return;
-            this.loading = true;
-            // fetch('http://localhost:3000/csvtoqr', {
-            //     method: 'post',
-            //     headers: {
-            //         'Accept': 'application/json',
-            //         'Content-Type': 'application/json'
-            //     },
-            //     body: JSON.stringify({
-            //         list: this.csv
-            //     })
-            // }).then(r => r.json()).then(images => {this.result = images, this.loading = false;});
+        submit(file) {
+            file.status++;
+            fetch(`${process.env.BACKEND_URL}/parsescanned`, {
+                method: 'post',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    base64: file.content
+                })
+            })
+                .then(r => r.json())
+                .then(r => {
+                    this.results.push({
+                        name: r.name.split(',').slice(-2).join(' '),
+                        sex: r.name.split(',')[1],
+                        category: r.name.split(',')[0],
+                        voies: Object.keys(r.results).filter(k => k.includes('v') && r.results[k] === 'yes').length,
+                        blocs: Object.keys(r.results).filter(k => k.includes('b') && r.results[k] === 'yes').length,
+                        raw: r.results
+                    });
+                    file.status++;
+                    this.updateQueue();
+                })
+                .catch(e => console.log(e));
+        },
+        updateQueue() {
+            if(this.files.filter(f => f.status === 1).length < 3 && this.files.filter(f => f.status === 0).length) {
+                this.submit(this.files.find(f => f.status === 0));
+            }
         }
     }
 };
